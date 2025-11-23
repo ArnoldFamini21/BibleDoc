e7e6f6e7d4433134ebd6a364b7109d2c56b4c307
ArnoldFamini21 <arnoldfamini21@gmail.com>
2025-11-23 10:07:54 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -75,13 +75,10 @@ add_action( 'widgets_init', 'bibledoc_widgets_init' );
 
 /**
  * Enqueue scripts and styles
- * VERSION 1.0.10 - Forces CSS refresh
+ * VERSION BUMPED TO 1.0.11
  */
 function bibledoc_scripts() {
-    // Main Stylesheet with version 1.0.10
-    wp_enqueue_style( 'bibledoc-style', get_stylesheet_uri(), array(), '1.0.10' ); 
-    
-    // Main Script
+    wp_enqueue_style( 'bibledoc-style', get_stylesheet_uri(), array(), '1.0.11' ); 
     wp_enqueue_script( 'bibledoc-script', get_template_directory_uri() . '/js/script.js', array(), '1.0.0', true );
 
     wp_localize_script( 'bibledoc-script', 'bibledocAjax', array(
@@ -214,7 +211,6 @@ function bibledoc_customize_register( $wp_customize ) {
 }
 add_action( 'customize_register', 'bibledoc_customize_register' );
 
-// Add defer to script
 function bibledoc_script_loader_tag( $tag, $handle ) {
     if ( 'bibledoc-script' === $handle ) return str_replace( ' src', ' defer src', $tag );
     return $tag;
